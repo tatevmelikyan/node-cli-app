3d14195ee88a6855aad2b71d3d67a08982c6c449
Tatev Melikyan <tatev.melikyan@ymail.com>
2023-05-31 20:16:08 +0400

    Made a cli app.

---
+++ app.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+import os from "os";
+import process from "node:process";
+
+const { username } = os.userInfo();
+console.log(`Welcome ${username}!`);
+
+process.on("exit", () => {
+  console.log(`Thank you ${username}, goodbye!`);
+});
+
+process.on("SIGINT", () => {
+  process.exit();
+});
+
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+  switch (input) {
+    case "os --cpus":
+      const cpus = os.cpus();
+      console.log(`CPUS amount: ${cpus.length}`);
+      cpus.forEach((cpu) => {
+        console.log(`model: ${cpu.model} speed: ${cpu.speed / 1000} GHz`);
+      });
+      break;
+    case "os --homedir":
+      const homedir = os.homedir();
+      console.log(homedir);
+      break;
+    case "os --username":
+      console.log(username);
+      break;
+    case "os --architecture":
+      const arch = os.arch();
+      console.log(arch);
+      break;
+    case "os --hostname":
+      const host = os.hostname();
+      console.log(host);
+      break;
+    case "os --platform":
+      const platform = os.platform();
+      console.log(platform);
+      break;
+    case "os --memory":
+      const totalMemory = os.totalmem();
+      console.log(totalMemory);
+      break;
+    case ".exit":
+      process.exit();
+    default:
+      console.log("Invalid input");
+  }
+});
